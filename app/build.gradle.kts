778f5c96eb8e818b3cdc7b41b74795f1deb36577
woheller69 <wolfgang.heller@web.de>
2025-01-30 11:23:30 +0100

    remove libonnxruntime.so and use "implementation" in Gradle instead

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("com.microsoft.onnxruntime:onnxruntime-android:1.17.1")
 }
